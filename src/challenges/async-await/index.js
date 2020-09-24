9fbe30f2446c7060d86fd9b01ee5510020cff7d4
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-09-24 16:43:09 -0300

    feat: module 3 solutions

--- src/challenges/async-await/index.js
+++ src/challenges/async-await/index.js
@@ -41,3 +41,34 @@ export const asyncGetUserFromGithub = async (user = "thiagojacinto") => {
   let response = await axios.get(`https://api.github.com/users/${user}`);
   console.log(response.data);
 };
+
+// 1-3
+export class Github {
+  static getRepositories(repo = "thiagojacinto/es6-review") {
+    axios
+      .get(`https://api.github.com/repos/${repo}`)
+      .then((response) => {
+        console.log(response.data.description);
+      })
+      .catch((err) => console.warn("Repository not found."));
+  }
+
+  static async asyncGetRepositories(repo = "thiagojacinto/es6-review") {
+    const response = await axios
+      .get(`https://api.github.com/repos/${repo}`)
+      .catch((err) => console.warn("Repository not found."));
+    console.log(response.data.description);
+  }
+}
+
+// 1-4
+export const buscaUsuario = (usuario = "thiagojacinto") => {
+  axios.get(`https://api.github.com/users/${usuario}`)
+    .then(response => console.log(response.data))
+    .catch(err => console.warn("User not found."));
+}
+
+export const asyncBuscaUsuario = async (usuario = "thiagojacinto") => {
+  const res = await axios.get(`https://api.github.com/users/${usuario}`);
+  console.log(res.data);
+}
