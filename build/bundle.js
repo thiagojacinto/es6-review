aa0e104ccc074282e6eb89528f989a4f8e0b9bad
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-09-24 09:57:25 -0300

    feat: js destructuring solutions

--- build/bundle.js
+++ build/bundle.js
@@ -1,81 +1,47 @@
 "use strict";
 
-// Arrow function conversion
-// 3.1
-var arr = [1, 2, 3, 4, 5];
-var ex31 = arr.map(function (item) {
-  return item + 10;
-});
-var solution31 = arr.map(function (item) {
-  return item + 10;
-});
-console.log("Solution 3.1:", solution31, ex31);
+/**
+ * DESTRUCTURING
+ */
+// 4.1
+var empresa = {
+  nome: "Rocketseat",
+  endereco: {
+    cidade: "Rio do Sul",
+    estado: "SC"
+  }
+};
+var nome = empresa.nome;
+var _empresa$endereco = empresa.endereco,
+    cidade = _empresa$endereco.cidade,
+    estado = _empresa$endereco.estado;
+console.group("4.1");
+console.log(nome);
+console.log(cidade);
+console.log(estado);
+console.groupEnd(); // 4.2
 
-var equalsArrays = function equalsArrays() {
-  var array1 = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
-  var array2 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
-  return array1.length === array2.length && array1.every(function (item, index) {
-    return item === array2[index];
-  });
+function mostraInfo(usuario) {
+  return "".concat(usuario.nome, " tem ").concat(usuario.idade, " anos.");
+}
+
+mostraInfo({
+  nome: "Diego",
+  idade: 23
+});
+
+var arrowMostraInfo = function arrowMostraInfo(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade;
+  return "".concat(nome, " tem ").concat(idade, " anos.");
 };
 
-console.assert(equalsArrays(ex31, solution31)); // 3.2
-
-var usuario = {
+var example = {
   nome: "Diego",
   idade: 23
 };
-
-function mostraIdade(usuario) {
-  return usuario.idade;
-}
-
-mostraIdade(usuario);
-
-var showAge = function showAge(usuario) {
-  return usuario.idade;
-};
-
-console.assert(mostraIdade(usuario) === showAge(usuario)); // 3.3
-
-var nome = "Diego";
-var idade = 23;
-
-function mostraUsuario() {
-  var nome = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "Diego";
-  var idade = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 18;
-  return {
-    nome: nome,
-    idade: idade
-  };
-}
-
-mostraUsuario(nome, idade);
-mostraUsuario(nome);
-
-var showUser = function showUser() {
-  var nome = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "Diego";
-  var idade = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 18;
-  return {
-    nome: nome,
-    idade: idade
-  };
-};
-
-console.assert(showUser(nome, idade), mostraUsuario(nome, idade));
-console.assert(showUser(nome), mostraUsuario(nome)); // 3.4
-
-var promise = function promise() {
-  return new Promise(function (resolve, reject) {
-    return resolve();
-  });
-};
-
-var arrowPromise = function arrowPromise() {
-  return new Promise(function (resolve, reject) {
-    return resolve();
-  });
-};
-
-promise().then(console.log("Regular function promise fulfilled."));
-arrowPromise().then(console.log("Arrow function promise fulfilled."));
+console.group("4.2");
+console.log(mostraInfo(example));
+console.log(arrowMostraInfo(example));
+console.assert(mostraInfo(example) === arrowMostraInfo(example));
+console.groupEnd();
